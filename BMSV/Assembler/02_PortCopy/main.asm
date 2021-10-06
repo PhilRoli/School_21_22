308e15cd830b9c7ffe0b40e7be4852e6e4839603
Philipp Rolinek <philipprolinek@gmail.com>
2021-10-06 08:46:53 +0200

    Finished script

--- BMSV/Assembler/02_PortCopy/main.asm
+++ BMSV/Assembler/02_PortCopy/main.asm
@@ -10,10 +10,25 @@
 ; PORTB is output
 ;
 
+; NOTES
+; 
 
 ; Replace with your application code
 start:
+; Input
+    ldi r16, 0x00
+    out DDRA, r16
+
+; Output
+    ldi r17, 0xFF
+    out DDRB, r17
+
     rjmp loop
 
 loop:
+    
+    in r16, PINA
+    out PORTB, r16
+    out PORTB, r17
+
     rjmp loop
