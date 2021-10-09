22c4fbc244fafff5ed750896471594cd52c4a34d
Philipp Rolinek <philipprolinek@gmail.com>
2021-10-10 00:12:08 +0200

    Added CS folder

---
+++ MGIN/pullFromGitLab.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd C:\VSC_School20-21\GitRepoTest
+
+git pull gitlab main
---
+++ MGIN/pushToGitLab.bat
@@ -0,0 +1,9 @@
+@echo off
+
+cd D:\Schul_Dokumente\4AHBG\MGIN
+
+git add .
+
+git commit -m "%date% - %time%"
+
+git push gitlab
