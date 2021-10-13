ee441abf8c0456d78028a4cbf5d38e8e32f7570f
Philipp Rolinek <philipprolinek@gmail.com>
2021-10-13 09:44:07 +0200

    Continued ExtInterrupt

--- BMSV/Assembler/04_Ext_Interupt/main.asm
+++ BMSV/Assembler/04_Ext_Interupt/main.asm
@@ -25,15 +25,34 @@ start:
 MainStart:
 ; Initialize the external interrupt
 ; Set stackpointer to the top of RAM
+; Stack -> Saves adress to jump back to after intrrupt
     ldi r16, high(RAMEND)
     out SPH, r16
     ldi r16, low(RAMEND)
     out SPL, r16
 ; Define and enable external interrupt 0
+; x register -> 16 bit wide
+; xh -> high byte
+; xl -> low byte
+    ldi xh, 0
+    ldi xl, EICRA
+; load -> write to register above 0x40 / r31
+    ld r16, x
+; set bits -> falling edge
+    ori r16, 0x02
+; 
+    st x, r16
+; load mask to r16
+    ldi r16, 1<<INT0
+; output mask to EIMSK
+    out EIMSK, r16
+; activate all interrupts
+    sei
 
 loop:
     inc r26
     rjmp loop
 
 Int0Function:
+    inc r20
     reti
