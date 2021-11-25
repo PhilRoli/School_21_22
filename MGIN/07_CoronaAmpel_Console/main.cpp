267cd31ee286822ed53e32d4623881d77f60437f
Philipp Rolinek <philipprolinek@gmail.com>
2021-11-25 16:08:02 +0100

    Last Lessons content

--- MGIN/07_CoronaAmpel_Console/main.cpp
+++ MGIN/07_CoronaAmpel_Console/main.cpp
@@ -1,25 +1,32 @@
 #include <QCoreApplication>
 #include <QFile>
-#include <QStringList>
 #include <iostream>
 
 using namespace std;
 
 struct GamesType
 {
-    // winner
     QString team1;
-    // looser
     QString team2;
-    // goals winner
     int goals1;
-    // goals looser
     int goals2;
 };
 
-QList<GamesType> procDta(QString str)
+QList<GamesType> procData(QString str)
 {
     QStringList strList = str.split("\n");
+    QStringList gameList = strList[0].split(",");
+
+    QString team1 = gameList[0];
+    QString team2 = gameList[1];
+    int goals1 = gameList[2].toInt();
+    int goals2 = gameList[3].toInt();
+
+    if (goals2 > goals1)
+    {
+        swap(goals1, goals2);
+        swap(team1, team2);
+    }
 }
 
 QString getDta(QString fName)
