e56062d4efe1f926b582e3cb3138f6485ade94c7
Philipp Rolinek <philipprolinek@gmail.com>
2021-12-02 17:45:52 +0100

    OOP a new

---
+++ MGIN/08_OopAllg/rectangle.h
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+#include <QString>
+
+class Rectangle
+{
+private:
+    int mL;
+    int mW;
+
+public:
+    Rectangle(int l, int w);
+    QString toString();
+    void setMl(int l);
+    void setMw(int w);
+    int getMl();
+    int getMw();
+    int calcA();
+    int calcU();
+};
+
+#endif // RECTANGLE_H
