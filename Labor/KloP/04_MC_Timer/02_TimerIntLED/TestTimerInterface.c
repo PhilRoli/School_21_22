53213c8063d0e22a74d8f1184fe38a8ed804e902
Philipp Rolinek <philipprolinek@gmail.com>
2022-01-06 16:10:12 +0100

    Updated program, not yet tested

--- Labor/KloP/04_MC_Timer/02_TimerIntLED/TestTimerInterface.c
+++ Labor/KloP/04_MC_Timer/02_TimerIntLED/TestTimerInterface.c
@@ -5,6 +5,12 @@
  * Author : Philipp Rolinek
  * Class  : 4AHBG
  * Description:
+ *      TimerVariable (mycounter) -> every 100us +1
+ *      Period Time = 10 000us -> 100 Timer Interrupts
+ *      Button Variable -> +1 = 100us longer on Time of LED
+ *      TimerVar = 0 -> LED on
+ *      TimerVar = ButtonVar -> LED out
+ *      TimerVar = 100 -> TimerVar = 0 -> LED on
  */
 
 #include <avr/interrupt.h>
@@ -14,9 +20,17 @@
 
 #define F_CPU 16000000UL
 
-// initialize deltatime
-volatile int schrittemax = 256;
-volatile int timeinc = 0;
+//* initialize variables
+//! All times in us
+// time between each timer interrupt
+volatile int timerPeriod = 100;
+// complete LED period time
+volatile int ledPeriod = 10000;
+// Max Num of timer interrupts befor LED off + max num of buttoninc
+// calc: ledPeriod / timerPeriod
+volatile int periodMax = 100;
+// Variable changed by up / down buttons
+volatile int buttoninc = 0;
 
 void TimerFunction(void *aUserData)
 {
@@ -28,17 +42,21 @@ int main(void)
 {
     TTimer timer;
     int mycounter = 0;
-    timer = TimerCreate(TIMER_NO_0, TIMER_MODE_NORMAL, 100, F_CPU);
+    timer = TimerCreate(TIMER_NO_0, TIMER_MODE_NORMAL, timerPeriod, F_CPU);
     TimerSetFunction(timer, TimerFunction, &mycounter);
 
-    // Registerys for Timer
+    //* Registerys for Interrupts
 
-    // Output of Signal on PortA
+    // Output of LED Signal on PortA pin 0
     DDRA |= (1 << 0);
 
-    // Set Interrupt to trigger on negative flank
+    // Output Counter Value
+    DDRB = 0xFF;
+
+    // Set button Interrupt to trigger on negative flank
     EICRA |= 0x0A;
-    // Enable int0 and int1 interrupts
+
+    // Enable int0 and int1 button interrupts
     // Int 0 -> UP | Int 1 -> Down
     EIMSK |= (1 << INT0);
     EIMSK |= (1 << INT1);
@@ -46,58 +64,47 @@ int main(void)
     // enable all interrupts
     sei();
 
-    // enable pull ups on interrupt pins
+    // enable pull ups on button interrupt pins
     PORTD |= (1 << 2);
     PORTD |= (1 << 3);
-
-    // Variables
-    //* period time in us, change last value in ms
-    int periodendauer = 1000 * 10;
-    // Higher delta time -> brighter LED
-    // int deltatime = periodendauer / schrittemax;
-
-    // Output Counter Value
-    DDRB = 0xFF;
-
-    // delta counter
-    int deltacounter;
-    int oldcounter = 0;
 
     /* Replace with your application code */
     while (1)
     {
-        deltacounter = mycounter - oldcounter;
-        PORTB = timeinc;
-        if (deltacounter < timeinc)
+        if (mycounter == 0)
         {
+            // if mycounter == 0 -> LED on
             PORTA |= (1 << 0);
         }
-        else
+        else if (mycounter == buttoninc)
         {
+            // if mycounter == buttoninc -> LED off
             PORTA &= ~(1 << 0);
         }
-
-        if (deltacounter > periodendauer)
+        else if (mycounter == periodMax)
         {
-            oldcounter = mycounter;
+            // if mycounter has reached period max -> reset to 0
+            mycounter = 0;
         }
+        // output current buttonInc value (7 LED's)
+        PORTB = buttoninc;
     }
 }
 
 ISR(INT0_vect)
 {
-    // UP
-    if (timeinc < schrittemax)
+    // UP if buttonInc in range of 0 - 100
+    if (buttoninc < periodMax)
     {
-        timeinc++;
+        buttoninc++;
     }
 }
 
 ISR(INT1_vect)
 {
-    // Down
-    if (timeinc > 0)
+    // Down if buttonInc in range of 0 - 100
+    if (buttoninc > 0)
     {
-        timeinc--;
+        buttoninc--;
     }
 }
