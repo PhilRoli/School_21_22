761fec4c1788d6ddff59aeeb9abc1ae02e920584
Philipp Rolinek <philipprolinek@gmail.com>
2022-02-24 17:40:59 +0100

    Finished + Misc

--- MGIN/17_IBAN/main.cpp
+++ MGIN/17_IBAN/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,7 +15,11 @@ public:
    */
   static bool isValid_ISBN(string nummerISBN)
   {
-    // ???????????
+    if (calcPruefziffer_ISBN(nummerISBN) == nummerISBN[nummerISBN.length() - 1])
+    {
+      return true;
+    }
+    return false;
   }
   /**
    * Berechnet aus der ISBN-13 Nummer die Prüfziffer
@@ -27,6 +32,20 @@ public:
     string nummer = getDigitsOnly(nummerISBN);
     // berechnet die Prüfziffer und gibt diese zurück
     // ????????????
+    int pruefZiffer = 0;
+    for (int i = 0; i < nummer.length(); i++)
+    {
+      if ((i + 1) % 2 == 0)
+      {
+        pruefZiffer += (nummer[i] - '0') * 3;
+      }
+      else
+      {
+        pruefZiffer += nummer[i] - '0';
+      }
+    }
+    pruefZiffer = pruefZiffer % 10;
+    return to_string(pruefZiffer)[0];
   }
 
 private:
@@ -34,16 +53,18 @@ private:
   // gibt einen string, der nur aus Ziffern besteht zurück
   static string getDigitsOnly(string nummer)
   {
-    // ???????????
+    nummer.erase(remove(nummer.begin(), nummer.end(), '-'), nummer.end());
+    return nummer;
   }
 };
 // ---------------------------------------------------------------
 int main()
 {
+  Pruefziffer pruefziffer;
   string isbn = string("978-0-30716-548-0");
   string isValid;
   cout << "ISBN: " << isbn << endl;
-  isValid = bool isValid_ISBN(isbn) ? "true" : "false";
+  isValid = (bool)pruefziffer.isValid_ISBN(isbn) ? "true" : "false";
   cout << "Pruefziffer ist " << isValid << endl;
   return 0;
 }
