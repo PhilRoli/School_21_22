49cb3dc51c166d3f98125b9a9e9ed2f5d763c0db
Philipp Rolinek <philipprolinek@gmail.com>
2022-03-07 12:35:57 +0100

    Added COmments

--- Labor/KloP/06_Lichtsensor/06_Lichtsensor/Source/TestRs232Simple.c
+++ Labor/KloP/06_Lichtsensor/06_Lichtsensor/Source/TestRs232Simple.c
@@ -23,39 +23,51 @@ int MyPut(char aCharacter, FILE *aFile)
 
 void ADCInit(void)
 {
-    ADMUX |= (3 << REFS0);  // AVCC with external capacitor
-    ADMUX |= (16 << MUX0);  // 1x gain
-    ADCSRA |= (1 << ADEN);  // Enables ADC
-    ADCSRA |= (1 << ADPS2); // Division factor of 16
+    // AVCC with external capacitor
+    ADMUX |= (3 << REFS0);
+    // 1x gain between ADC0 and ADC1
+    ADMUX |= (16 << MUX0);
+    // Enables ADC
+    ADCSRA |= (1 << ADEN);
+    // Division factor of 16
+    ADCSRA |= (1 << ADPS2);
+    // Activates the ADC
     PRR &= ~(1 << PRADC);
+    // Left Adjust Result
     ADMUX &= ~(1 << ADLAR);
 }
 
 unsigned int getvalue(void)
 {
     unsigned int value;
-
+    // Start ADC Conversion
     ADCSRA |= (1 << ADSC);
-
+    // Wait until conversion is finished
     while (ADCSRA & (1 << ADSC))
         ;
 
-    value = ADCL; // Low
+    // Read lower part of ADC converted value
+    value = ADCL;
+    // Read hig part of value
     value |= ((unsigned int)ADCH << 8);
     return value;
 }
 
 int main(void)
 {
+    // Set port A as input
     DDRA = 0x00;
 
+    // Initialize ADC converter and Rs232 connection
     ADCInit();
     Rs232Init();
 
+    // fdevopen() gets called by "printf()"
     fdevopen(MyPut, NULL);
 
     while (1)
     {
+        // Print value to Rs232 connection
         printf("%d ", getvalue());
 
         _delay_ms(500);
