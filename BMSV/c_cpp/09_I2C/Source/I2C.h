ab86734339503f4c9767e34ba764275bb6a18b5f
Philipp Rolinek <philipprolinek@gmail.com>
2022-03-21 13:45:41 +0100

    I2C Bus Controller Progamm Intial Commit

---
+++ BMSV/c_cpp/09_I2C/Source/I2C.h
@@ -0,0 +1,9 @@
+/*
+ * I2C.h
+ *
+ * Created: 21.03.2022 13:43:25
+ * Author: Philipp Rolinek
+ * Class: 4AHBG
+ * Description:
+ *		I2C Bus Programm
+ */
---
+++ BMSV/c_cpp/09_I2C/Source/TestI2C.c
@@ -0,0 +1,9 @@
+/*
+ * TestI2C.c
+ *
+ * Created: 21.03.2022 13:43:37
+ * Author: Philipp Rolinek
+ * Class: 4AHBG
+ * Description:
+ *		I2C Bus Controller Programm
+ */
